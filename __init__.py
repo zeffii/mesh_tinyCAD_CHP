de3ba9ee25a15aa5169b5d5c65804050e32f7c12
zeffii <tetha.z@gmail.com>
2016-01-24 19:15:32 +0100

    added menu attachment

--- __init__.py
+++ __init__.py
@@ -1,3 +1,31 @@
+"""
+2016 Dealga McArdle | zeffii@hotmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""  
+
+
+bl_info = {
+    "name": "CHP addon for tinycad",
+    "description": "this tinycad add-on performs VTX-V and triggers Fillet in modal mode afterwards",
+    "author": "Dealga McArdle",
+    "version": (0, 1),
+    "blender": (2, 7, 6),
+    "category": "Mesh"
+}
+
+
 import math
 
 import bpy
@@ -97,11 +125,21 @@ class TCChamferPlus(bpy.types.Operator):
         return {"CANCELLED"}
 
 
+def menu_func(self, context):
+    operator = self.layout.operator
+    operator('tinycad.chamfer_plus', text='CHP | Chamfer+ (VTX-V /w Fillet)')
+
+
 def register():
     bpy.utils.register_module(__name__)
+    try:
+        bpy.types.VIEW3D_MT_edit_mesh_tinycad.append(menu_func)
+    except:
+        print('mesh_tinyCAD menu not found, cannot add')
 
 def unregister():
     bpy.utils.unregister_module(__name__)
-
-if __name__ == "__main__":
-    register()
+    try:
+        bpy.types.VIEW3D_MT_edit_mesh_tinycad.remove(menu_func)
+    except:
+        ...
