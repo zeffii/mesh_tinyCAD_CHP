ae0170d0bdc9ebc93dc5624c7e793dd478ec5587
zeffii <tetha.z@gmail.com>
2016-01-24 15:07:58 +0100

    updated operator

--- __init__.py
+++ __init__.py
@@ -11,8 +11,21 @@ from mathutils.geometry import intersect_point_line as PtLineIntersect
 
 
 def operate(context, bm, selected):
-    print(selected)
+    edge_1, edge_2 = selected
+    edge_1_co_1 = edge_1.verts[0].co
+    edge_1_co_2 = edge_1.verts[1].co
+    edge_2_co_1 = edge_2.verts[0].co
+    edge_2_co_2 = edge_2.verts[1].co
+    isect = LineIntersect(edge_1_co_1, edge_1_co_2, edge_2_co_1, edge_2_co_2)
 
+    if (not isect) or ((isect[0] - isect[1]).length >= 1.0e-5):
+        print('these edges do not intersect')
+        return
+    else:    
+        print('definite intersection found')
+
+    
+    
 
 class TCChamferPlus(bpy.types.Operator):
     bl_idname = "tinycad.chamfer_plus"
